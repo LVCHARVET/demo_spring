9e563e4668dcc31ce5881e19b98b54eef263d443
LVCHARVET <lv.charvet28@gmail.com>
2024-04-09 15:08:27 +0200

    TP04

--- src/main/java/com/example/demo/rest/VilleControleur.java
+++ src/main/java/com/example/demo/rest/VilleControleur.java
@@ -3,23 +3,46 @@ package com.example.demo.rest;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.model.Ville;
 
+import jakarta.annotation.PostConstruct;
+
 @RestController
 @RequestMapping("/villes")
 public class VilleControleur {
 
-	@GetMapping	
-	public List<Ville> listeVille() {
-		List<Ville> villes = new ArrayList<>();
+    private List<Ville> villes;
+
+    @PostConstruct
+    public void init() {
+        villes = new ArrayList<>();
         villes.add(new Ville("Paris", 2243833));
         villes.add(new Ville("Marseille", 860363));
         villes.add(new Ville("Lyon", 513275));
+    }
+
+    @GetMapping
+    public List<Ville> listeVille() {
         return villes;
-	}
-	
+    }
+
+    @PostMapping
+    public ResponseEntity<String> addVille(@RequestBody Ville nouvelleVille) {
+        for (Ville ville : villes) {
+            if (ville.getNom().equals(nouvelleVille.getNom())) {
+                return ResponseEntity.status(HttpStatus.CONFLICT).body("La ville est déjà enregistrée");
+            }
+        }
+
+        villes.add(nouvelleVille);
+        return ResponseEntity.status(HttpStatus.CREATED).body("La ville a été ajoutée avec succès !");
+    }
 }
