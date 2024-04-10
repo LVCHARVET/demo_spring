852ebf6bf419a2ddbc33840bf0407ed00b8a4a36
LVCHARVET <lv.charvet28@gmail.com>
2024-04-10 11:00:01 +0200

    TP06

--- src/main/java/com/example/demo/service/VilleService.java
+++ src/main/java/com/example/demo/service/VilleService.java
@@ -8,59 +8,68 @@ import org.springframework.stereotype.Service;
 import com.example.demo.model.Ville;
 import com.example.demo.repository.VilleRepository;
 
+import jakarta.annotation.PostConstruct;
+
 @Service
 public class VilleService {
 
 	@Autowired
 	private VilleRepository villeRepository;
 
+	@PostConstruct
+	public void init() {
+		villeRepository.save(new Ville("Paris", 2243833));
+		villeRepository.save(new Ville("Marseille", 860363));
+		villeRepository.save(new Ville("Lyon", 513275));
+	}
+
 	public List<Ville> extractVilles() {
-		
+
 		return villeRepository.findAll();
-		
+
 	}
 
 	public Ville extractVille(int idVille) {
-		
+
 		return villeRepository.findById(idVille).orElse(null);
-		
+
 	}
 
 	public Ville extractVille(String nom) {
-		
+
 		return villeRepository.findByNom(nom);
-		
+
 	}
 
 	public List<Ville> insertVille(Ville ville) {
-		
+
 		villeRepository.save(ville);
-		
+
 		return extractVilles();
-		
+
 	}
 
 	public List<Ville> modifierVille(int idVille, Ville villeModifiee) {
-		
+
 		Ville ville = villeRepository.findById(idVille).orElse(null);
-		
+
 		if (ville != null) {
-			
+
 			ville.setNom(villeModifiee.getNom());
 			ville.setNbHabitants(villeModifiee.getNbHabitants());
 			villeRepository.save(ville);
-			
+
 		}
-		
+
 		return extractVilles();
-		
+
 	}
 
 	public List<Ville> supprimerVille(int idVille) {
-		
+
 		villeRepository.deleteById(idVille);
-		
+
 		return extractVilles();
-		
+
 	}
 }
